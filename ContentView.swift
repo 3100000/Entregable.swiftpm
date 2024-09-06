4015e3ec11b24e84216e40111b33b18517143294
ktarzyz <kt@gmail.com>
2024-09-06 10:32:24 -0600

    Cree un textinput

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Variable que contiene el texto, se actualiza automáticamente
+    @State private var message = "Hola, que bueno verte!"
+    @State private var message2 = "El tema de "
+    
+    // Variable para almacenar el texto ingresado por el usuario
+    @State private var inputText: String = ""
+    // Variable para mostrar el resultado cuando se presiona el botón
+    @State private var outputText: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, worlds!")
+        VStack(spacing: 20) { // Apila los elementos verticalmente
+            Text(message) // Muestra el mensaje actual
+                .font(.largeTitle) // Tamaño de la fuente
+                .padding() // Agrega espacio alrededor del texto
+            
+            TextField("¿Que te gustaría aprender?", text: $inputText)
+                .padding()
+                .border(Color.gray, width: 1)
+                .padding()
+            
+            
+            
+            
+            Button(action: {
+                // Acción que se ejecuta cuando se presiona el botón
+                message = "El tema de" // Cambia el texto
+            }) {
+                Text("Buscar") // Texto que se muestra en el botón
+                    .font(.title) // Tamaño de la fuente del botón
+                    .padding() // Agrega espacio alrededor del texto del botón
+                    .background(Color.green) // Fondo azul para el botón
+                    .foregroundColor(.white) // Color de texto blanco
+                    .cornerRadius(10) // Bordes redondeados
+            }
         }
     }
 }
+
