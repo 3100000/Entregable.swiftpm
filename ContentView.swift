c69d19523ad15b2869a43e1349e7faa1d83427de
ktarzyz <kt@gmail.com>
2024-09-06 10:47:12 -0600

    El boton para imprimir el texto ya funciona

--- ContentView.swift
+++ ContentView.swift
@@ -21,21 +21,23 @@ struct ContentView: View {
                 .border(Color.gray, width: 1)
                 .padding()
             
-            
-            
-            
             Button(action: {
-                // Acción que se ejecuta cuando se presiona el botón
-                message = "El tema de" // Cambia el texto
-            }) {
-                Text("Buscar") // Texto que se muestra en el botón
-                    .font(.title) // Tamaño de la fuente del botón
-                    .padding() // Agrega espacio alrededor del texto del botón
-                    .background(Color.green) // Fondo azul para el botón
-                    .foregroundColor(.white) // Color de texto blanco
-                    .cornerRadius(10) // Bordes redondeados
+                outputText = "Será un placer ayudarte con el tema de " + inputText
+            }){
+                Text("Buscar") // Texto del botón
+                    .font(.title) // Tamaño de la fuente
+                    .padding() // Agrega espacio alrededor del texto
+                    .background(Color.blue) // Fondo azul
+                    .foregroundColor(.white) // Texto blanco
+                    .cornerRadius(10)
             }
+            
+            Text(outputText)
+                .font(.title2) // Tamaño del texto
+                .padding() // Agrega espacio alrededor del texto
+                .foregroundColor(.black) // Color de texto negro
+            }
+            .padding()
         }
     }
-}
 
